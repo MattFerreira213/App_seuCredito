910d5d0c97905c7528c0ed217e5c7c512ce1a732
Mateus Ferreira <mateusanjos225@gmail.com>
2021-06-21 00:20:09 -0300

    Finalização das telas de opções de pagamento e implementação da regra de negocio

--- app/src/main/java/com/afcompany/seucredito/Proposta2Activity.java
+++ app/src/main/java/com/afcompany/seucredito/Proposta2Activity.java
@@ -1,19 +1,183 @@
 package com.afcompany.seucredito;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import java.math.BigDecimal;
+
+import static android.widget.Toast.LENGTH_SHORT;
+
 public class Proposta2Activity extends AppCompatActivity {
+
+    private EditText edtParcelas;
+    private TextView txtOpcao1;
+    private TextView txtOpcao2;
+    private TextView txtOpcao3;
+    private TextView txtOpcao4;
+
+    private Button btnOpcoesParcelas;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_proposta_2);
-
+        setContentView(R.layout.activity_segunda_opcao_pagamento);
         Toolbar();
+
+        edtParcelas = findViewById(R.id.edt_parcelas);
+
+        txtOpcao1 = findViewById(R.id.txt_opcao1);
+        txtOpcao2 = findViewById(R.id.txt_opcao2);
+        txtOpcao3 = findViewById(R.id.txt_opcao3);
+        txtOpcao4 = findViewById(R.id.txt_opcao4);
+
+        txtOpcao1.setVisibility(View.GONE);
+        txtOpcao2.setVisibility(View.GONE);
+        txtOpcao3.setVisibility(View.GONE);
+        txtOpcao4.setVisibility(View.GONE);
+
+        btnOpcoesParcelas = findViewById(R.id.btn_opcoes_pagamento);
+        btnOpcoesParcelas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (edtParcelas.getText().toString().isEmpty()) {
+                    Toast.makeText(Proposta2Activity.this, R.string.error_msg3, LENGTH_SHORT).show();
+                    return;
+                }
+
+                String sParcelas = edtParcelas.getText().toString();
+                String sValorEmprestimo = getIntent().getStringExtra("valorEmprestimo");
+                int parcelas = Integer.parseInt(sParcelas);
+                double valorEmprestimo = Double.parseDouble(sValorEmprestimo);
+                double valorFinal;
+
+                if (parcelas >= 1 && parcelas <= 12) {
+                    if (parcelas == 1) {
+                        valorFinal = (valorEmprestimo * 0.035) + valorEmprestimo;
+
+                        BigDecimal pagamento = BigDecimal.valueOf(valorFinal);
+                        pagamento = pagamento.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+                        txtOpcao1.setText("R$" + pagamento);
+                        txtOpcao1.setVisibility(View.VISIBLE);
+
+                        txtOpcao2.setVisibility(View.GONE);
+                        txtOpcao3.setVisibility(View.GONE);
+                        txtOpcao4.setVisibility(View.GONE);
+                    } else if (parcelas == 2) {
+                        valorFinal = (valorEmprestimo * 2 * 0.035) + valorEmprestimo;
+
+                        BigDecimal pagamento = BigDecimal.valueOf(valorFinal / 2);
+                        pagamento = pagamento.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+                        txtOpcao1.setText("2x de R$" + pagamento);
+                        txtOpcao1.setVisibility(View.VISIBLE);
+
+                        txtOpcao2.setVisibility(View.GONE);
+                        txtOpcao3.setVisibility(View.GONE);
+                        txtOpcao4.setVisibility(View.GONE);
+                    } else {
+                        valorFinal = (valorEmprestimo * parcelas * 0.035) + valorEmprestimo;
+
+                        BigDecimal pagamentoOpcao1 = BigDecimal.valueOf(valorFinal / parcelas);
+                        BigDecimal pagamentoOpcao2 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.1)) / (parcelas - 1));
+                        BigDecimal pagamentoOpcao3 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.2)) / (parcelas - 1));
+                        BigDecimal pagamentoOpcao4 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.3)) / (parcelas - 1));
+
+                        pagamentoOpcao1 = pagamentoOpcao1.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                        pagamentoOpcao2 = pagamentoOpcao2.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                        pagamentoOpcao3 = pagamentoOpcao3.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                        pagamentoOpcao4 = pagamentoOpcao4.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+                        BigDecimal primeiroPagamentoOpcao2 = BigDecimal.valueOf(valorFinal * 0.1);
+                        BigDecimal primeiroPagamentoOpcao3 = BigDecimal.valueOf(valorFinal * 0.2);
+                        BigDecimal primeiroPagamentoOpcao4 = BigDecimal.valueOf(valorFinal * 0.3);
+
+                        primeiroPagamentoOpcao2 = primeiroPagamentoOpcao2.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+                        primeiroPagamentoOpcao3 = primeiroPagamentoOpcao3.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+                        primeiroPagamentoOpcao4 = primeiroPagamentoOpcao4.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+
+                        txtOpcao1.setText(parcelas + "x de R$" + pagamentoOpcao1);
+                        txtOpcao2.setText("1x de R$" + primeiroPagamentoOpcao2 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao2);
+                        txtOpcao3.setText("1x de R$" + primeiroPagamentoOpcao3 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao3);
+                        txtOpcao4.setText("1x de R$" + primeiroPagamentoOpcao4 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao4);
+
+                        txtOpcao1.setVisibility(View.VISIBLE);
+                        txtOpcao2.setVisibility(View.VISIBLE);
+                        txtOpcao3.setVisibility(View.VISIBLE);
+                        txtOpcao4.setVisibility(View.VISIBLE);
+                    }
+                } else if(parcelas > 12 && parcelas <= 36){
+                    valorFinal = (valorEmprestimo * parcelas * 0.042) + valorEmprestimo;
+
+                    BigDecimal pagamentoOpcao1 = BigDecimal.valueOf(valorFinal / parcelas);
+                    BigDecimal pagamentoOpcao2 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.1)) / (parcelas - 1));
+                    BigDecimal pagamentoOpcao3 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.2)) / (parcelas - 1));
+                    BigDecimal pagamentoOpcao4 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.3)) / (parcelas - 1));
+
+                    pagamentoOpcao1 = pagamentoOpcao1.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                    pagamentoOpcao2 = pagamentoOpcao2.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                    pagamentoOpcao3 = pagamentoOpcao3.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                    pagamentoOpcao4 = pagamentoOpcao4.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+                    BigDecimal primeiroPagamentoOpcao2 = BigDecimal.valueOf(valorFinal * 0.1);
+                    BigDecimal primeiroPagamentoOpcao3 = BigDecimal.valueOf(valorFinal * 0.2);
+                    BigDecimal primeiroPagamentoOpcao4 = BigDecimal.valueOf(valorFinal * 0.3);
+
+                    primeiroPagamentoOpcao2 = primeiroPagamentoOpcao2.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+                    primeiroPagamentoOpcao3 = primeiroPagamentoOpcao3.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+                    primeiroPagamentoOpcao4 = primeiroPagamentoOpcao4.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+
+                    txtOpcao1.setText(parcelas + "x de R$" + pagamentoOpcao1);
+                    txtOpcao2.setText("1x de R$" + primeiroPagamentoOpcao2 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao2);
+                    txtOpcao3.setText("1x de R$" + primeiroPagamentoOpcao3 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao3);
+                    txtOpcao4.setText("1x de R$" + primeiroPagamentoOpcao4 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao4);
+
+                    txtOpcao1.setVisibility(View.VISIBLE);
+                    txtOpcao2.setVisibility(View.VISIBLE);
+                    txtOpcao3.setVisibility(View.VISIBLE);
+                    txtOpcao4.setVisibility(View.VISIBLE);
+                } else if( parcelas > 36){
+                    valorFinal = (valorEmprestimo * parcelas * 0.05) + valorEmprestimo;
+
+                    BigDecimal pagamentoOpcao1 = BigDecimal.valueOf(valorFinal / parcelas);
+                    BigDecimal pagamentoOpcao2 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.1)) / (parcelas - 1));
+                    BigDecimal pagamentoOpcao3 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.2)) / (parcelas - 1));
+                    BigDecimal pagamentoOpcao4 = BigDecimal.valueOf((valorFinal - (valorFinal * 0.3)) / (parcelas - 1));
+
+                    pagamentoOpcao1 = pagamentoOpcao1.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                    pagamentoOpcao2 = pagamentoOpcao2.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                    pagamentoOpcao3 = pagamentoOpcao3.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+                    pagamentoOpcao4 = pagamentoOpcao4.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+
+                    BigDecimal primeiroPagamentoOpcao2 = BigDecimal.valueOf(valorFinal * 0.1);
+                    BigDecimal primeiroPagamentoOpcao3 = BigDecimal.valueOf(valorFinal * 0.2);
+                    BigDecimal primeiroPagamentoOpcao4 = BigDecimal.valueOf(valorFinal * 0.3);
+
+                    primeiroPagamentoOpcao2 = primeiroPagamentoOpcao2.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+                    primeiroPagamentoOpcao3 = primeiroPagamentoOpcao3.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+                    primeiroPagamentoOpcao4 = primeiroPagamentoOpcao4.setScale(2,BigDecimal.ROUND_HALF_EVEN);
+
+                    txtOpcao1.setText(parcelas + "x de R$" + pagamentoOpcao1);
+                    txtOpcao2.setText("1x de R$" + primeiroPagamentoOpcao2 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao2);
+                    txtOpcao3.setText("1x de R$" + primeiroPagamentoOpcao3 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao3);
+                    txtOpcao4.setText("1x de R$" + primeiroPagamentoOpcao4 + " + " + (parcelas - 1) + "x de R$" + pagamentoOpcao4);
+
+                    txtOpcao1.setVisibility(View.VISIBLE);
+                    txtOpcao2.setVisibility(View.VISIBLE);
+                    txtOpcao3.setVisibility(View.VISIBLE);
+                    txtOpcao4.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
     }
 
     private void Toolbar() {
--- app/src/main/java/com/afcompany/seucredito/SolicitacaoActivity.java
+++ app/src/main/java/com/afcompany/seucredito/SolicitacaoActivity.java
@@ -1,6 +1,8 @@
 package com.afcompany.seucredito;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +21,6 @@ public class SolicitacaoActivity extends AppCompatActivity {
     private EditText edtIdade;
     private EditText edtRendaMensal;
     private EditText edtValorEmprestimo;
-    private EditText edtParcelas;
 
     private static final double SALARIO_MINIMO = 1100.00;
     private static final int IDADE = 18;
@@ -36,14 +37,13 @@ public class SolicitacaoActivity extends AppCompatActivity {
         edtIdade = findViewById(R.id.edt_idade);
         edtRendaMensal = findViewById(R.id.edt_renda_mensal);
         edtValorEmprestimo = findViewById(R.id.edt_valor_emprestimo);
-        edtParcelas = findViewById(R.id.edt_parcelas);
 
-        Button buttonOfertas = findViewById(R.id.btn_oferta_detalhes);
+        Button buttonOfertas = findViewById(R.id.btn_oferta_credito);
         buttonOfertas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (validate()){
+                if (validate()) {
                     Toast.makeText(SolicitacaoActivity.this, R.string.error_msg2, LENGTH_SHORT).show();
                     return;
                 }
@@ -57,19 +57,27 @@ public class SolicitacaoActivity extends AppCompatActivity {
                 int idade = Integer.parseInt(sIdade);
 
 
-                if(idade >= IDADE && rendaMesnal >= SALARIO_MINIMO) {
-                    if(rendaMesnal >= SALARIO_MINIMO && rendaMesnal < (3 * SALARIO_MINIMO) && valorEmprestimo >= 150 && valorEmprestimo <= 4400){
+                if (idade >= IDADE && rendaMesnal >= SALARIO_MINIMO) {
+                    if (rendaMesnal >= SALARIO_MINIMO && rendaMesnal < (3 * SALARIO_MINIMO) && valorEmprestimo >= 150 && valorEmprestimo <= 4400) {
                         Intent in = new Intent(SolicitacaoActivity.this, Proposta1Activity.class);
+                        in.putExtra("valorEmprestimo", edtValorEmprestimo.getText().toString());
                         startActivity(in);
-                    } else if (rendaMesnal >= (3 * SALARIO_MINIMO) && rendaMesnal < (6 * SALARIO_MINIMO) && valorEmprestimo > 4400 && valorEmprestimo <= 13200){
+                        finish();
+                    } else if (rendaMesnal >= (3 * SALARIO_MINIMO) && rendaMesnal < (6 * SALARIO_MINIMO) && valorEmprestimo > 4400 && valorEmprestimo <= 13200) {
                         Intent in = new Intent(SolicitacaoActivity.this, Proposta2Activity.class);
+                        in.putExtra("valorEmprestimo", edtValorEmprestimo.getText().toString());
                         startActivity(in);
-                    } else if (rendaMesnal >= (6 * SALARIO_MINIMO) && rendaMesnal < (8 * SALARIO_MINIMO) && valorEmprestimo > 13200 && valorEmprestimo <= 26400){
+                        finish();
+                    } else if (rendaMesnal >= (6 * SALARIO_MINIMO) && rendaMesnal < (8 * SALARIO_MINIMO) && valorEmprestimo > 13200 && valorEmprestimo <= 26400) {
                         Intent in = new Intent(SolicitacaoActivity.this, Proposta3Activity.class);
+                        in.putExtra("valorEmprestimo", edtValorEmprestimo.getText().toString());
                         startActivity(in);
-                    } else if (rendaMesnal >= (8 * SALARIO_MINIMO) && valorEmprestimo > 26400 && valorEmprestimo <= 40000){
+                        finish();
+                    } else if (rendaMesnal >= (8 * SALARIO_MINIMO) && valorEmprestimo > 26400 && valorEmprestimo <= 40000) {
                         Intent in = new Intent(SolicitacaoActivity.this, Proposta4Activity.class);
+                        in.putExtra("valorEmprestimo", edtValorEmprestimo.getText().toString());
                         startActivity(in);
+                        finish();
                     } else {
                         Toast.makeText(SolicitacaoActivity.this, R.string.msg_negado, LENGTH_SHORT).show();
                         return;
@@ -92,6 +100,6 @@ public class SolicitacaoActivity extends AppCompatActivity {
     private boolean validate(){
         return(edtIdade.getText().toString().isEmpty() && edtRendaMensal.getText().toString().isEmpty()
                 && edtNome.getText().toString().isEmpty() && edtCpf.getText().toString().isEmpty()
-                && edtValorEmprestimo.getText().toString().isEmpty() && edtParcelas.getText().toString().isEmpty());
+                && edtValorEmprestimo.getText().toString().isEmpty());
     }
 }
